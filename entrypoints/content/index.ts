b312a79c7132a23cdc4b79299b41412b57cb5b72
Cedric <14017092+douyixuan@users.noreply.github.com>
2024-12-21 15:34:31 +0800

    fix: add button to twitter profile

--- entrypoints/content/index.ts
+++ entrypoints/content/index.ts
@@ -27,9 +27,89 @@ export default defineContentScript({
   },
 });
 
-function addProfileBtn(container: HTMLElement, _shadow: ShadowRoot, shadowHost: HTMLElement) {
+/**
+ *
+ * @param shadowRootContainer - The HTML element that is the shadowRoot's parent
+ * @param portalRoot - The HTML element that you want Modals to be teleported to
+ * @returns
+ */
+function patchPortalRoot(
+  shadowRootContainer: HTMLElement,
+  portalRoot?: HTMLElement,
+) {
+  const elementById = Document.prototype.getElementById
+
+  const element = portalRoot || shadowRootContainer.shadowRoot?.children[0]
+  if (!element)
+    return
+
+  Document.prototype.getElementById = function (elementId: string) {
+    if (elementId === 'headlessui-portal-root') {
+      const d = document.createElement('div')
+      d.id = 'headlessui-portal-root'
+      element.appendChild(d)
+      return d
+    }
+    return elementById.call(this, elementId)
+  }
+
+  const activeElementDescriptorGetter = Object.getOwnPropertyDescriptor(
+    Document.prototype,
+    'activeElement',
+  )?.get
+
+  Object.defineProperty(Document.prototype, 'activeElement', {
+    get() {
+      const activeElement = activeElementDescriptorGetter?.call(this)
+      if (activeElement === shadowRootContainer)
+        return shadowRootContainer.shadowRoot?.activeElement
+    },
+  })
+
+  const targetGetter = Object.getOwnPropertyDescriptor(
+    Event.prototype,
+    'target',
+  )?.get
+
+  Object.defineProperty(Event.prototype, 'target', {
+    get() {
+      const target = targetGetter?.call(this)
+
+      if (target === shadowRootContainer && this.path)
+        return this.path[0]
+
+      return target
+    },
+  })
+}
+
+const setupApp = app => {
+  const tabId = ref('')
+  app.provide('tabId', tabId)
+}
+
+function addProfileBtn() {
   let href = ''
-  let interval = 0
+  let interval = 1
+  let __DEV__ = false
+  console.log('====> startInjectProfileBtnInterval :')
+
+  const container = document.createElement('div')
+  container.id = `profile-url-buy-btn`
+  const root = document.createElement('div')
+  patchPortalRoot(container, root)
+  const styleEl = document.createElement('link')
+  const shadowDOM = container.attachShadow?.({ mode: __DEV__ ? 'open' : 'closed' }) || container
+  styleEl.setAttribute('rel', 'stylesheet')
+  styleEl.setAttribute('href', browser.runtime.getURL('dist/contentScripts/style.css'))
+  shadowDOM.appendChild(styleEl)
+  shadowDOM.appendChild(root)
+  const app = createApp(BuyButton)
+  setupApp(app)
+  const profileId = ref('')
+  app.provide('profileId', profileId)
+  app.mount(root)
+
   const startInjectProfileBtnInterval = () => {
     console.log('====> startInjectProfileBtnInterval :')
     let intervalTimes = 1
@@ -43,19 +123,26 @@ function addProfileBtn(container: HTMLElement, _shadow: ShadowRoot, shadowHost: 
       }
       intervalTimes++
 
-      // detect user profile website
-      // const profileInTwitter = document.querySelector('[data-testid="UserProfileHeader_Items"]')
-      // if (!profileInTwitter) return
+      const descInTwitter = document.querySelector('[data-testid="UserDescription"]')
+      if (!descInTwitter) return
 
-      // const userUrl = document.querySelector('[data-testid="UserUrl"]')
-      // console.log('====> userurl :', userUrl.innerText)
+      const descText = descInTwitter.innerText
+      const index = descText.indexOf('rwa-wallet.com')
+      if (index === -1) return
+      
+      let nftId = descText.substr(index).replace('rwa-wallet.com/', '')
+      nftId = nftId.split(' ')[0]
+      // if (!nftId) return
+      profileId.value = nftId
+      console.log('====> descText, nftId :', descText, nftId)
+      const usernameNode = document.querySelector('[data-testid="UserName"]')
+      const avatarRowNode = usernameNode.previousSibling
 
       clearInterval(interval)
-      // add a button
-      const app = createApp(BuyButton);
-      app.mount(container);
-      shadowHost.style.pointerEvents = "none";
-      return app;
+
+      const parentNode = avatarRowNode?.lastChild
+      const firstChild = parentNode.firstChild
+      parentNode.insertBefore(container, firstChild)
     }, intervalTimeSpan)
   }
 
@@ -65,6 +152,7 @@ function addProfileBtn(container: HTMLElement, _shadow: ShadowRoot, shadowHost: 
       href = location.href
       if (interval) {
         console.log('====> clearInterval in setInterval :', interval)
+        profileId.value = ''
         clearInterval(interval)
       }
       startInjectProfileBtnInterval()
